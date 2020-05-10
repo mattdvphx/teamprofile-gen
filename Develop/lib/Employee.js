580a68000e6b4ef4040500b64796acf47209c2da
Joao M Dalla Vecchia <59539338+mattdvphx@users.noreply.github.com>
2020-05-10 12:40:48 -0700

    added func

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,21 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email){
+        this.id = id;
+    this.name = name;
+    this.email = email;
+    }
+    getName(){
+        return this.name;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getId(){
+        return this.id;
+    }
+    getRole(){
+        return "Employee";
+    }
+}
+module.exports = Employee;
